2e61bc305afaf8ac8ba5947517b6fdcb00f064fc
mkushnikov <kushnikov.mikhail@gmail.com>
2020-03-12 00:10:37 +0200

    Demo scene setting up.
    - created demo level
    - developed enemy forward moving
    - created base interface to spawn enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+
+        public float MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
+
+        private void Update()
+        {
+            transform.position += Vector3.right * MoveSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Spawner
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _enemyPrefab;
+        [SerializeField] private int _waveSize;
+        [SerializeField] private float _spawnRate;
+
+        private void Start()
+        {
+            if (_enemyPrefab != null)
+            {
+                StartCoroutine(SpawnWave(_enemyPrefab, _waveSize, _spawnRate));
+            }
+        }
+
+        public IEnumerator SpawnWave(GameObject spawningPrefab, int number, float spawnRate)
+        {
+            Debug.Log("CR started");
+            while (number-- != 0)
+            {
+                Debug.Log("Spawn");
+                Instantiate(spawningPrefab, transform.position, Quaternion.identity);
+                if (number != 0)
+                {
+                    yield return new WaitForSeconds(spawnRate);
+                }
+            }
+            Debug.Log("CR ended");
+        }
+    }
+}
